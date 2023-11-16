0e3f3fef0cd60de7784ac6f781ee7ce350b3715d
Jinad Tope Isaac <jinadtope66@gmailcom>
2023-11-16 09:07:26 +0100

    updated

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,5 +1,9 @@
 
 const userModel = require("../Models/user.model")
+const bcryptjs = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+const secret = process.env.secret
+
 const userWelcome = (req, res)=>{
     res.send("hello jinad")
 }
@@ -39,12 +43,54 @@ const signUp =(req,res)=>{
     .catch((err) => {
       console.log(err);
       if (err.code == 11000) {
-        res.render("signUp", { message: "This email has already been used" });
+        res.send({status:false, message: "This email has already been used" });
       } else {
-        res.render("signUp", { message: "An error occurred during sign Up!" });
+      
+        res.send({status:false, message: "An error occurred during sign Up!" });
       }
     });
 }
 
-module.exports = {userWelcome, registerUser, signUp }
+const authenticateUser = (req,res)=>{
+  console.log(req.body);
+  userModel.findOne({email:req.body.email})
+  .then((result)=>{
+    if(result){
+      console.log(result);
+      bcryptjs.compare(req.body.password, result.password,(err,response)=>{
+        if (response) {
+          console.log(response);
+          let token =   jwt.sign({email:req.body.email}, secret, {expiresIn:"3hr"})
+          console.log(token);
+          res.send({status:true ,message: "login successful", token})
+        }else{
+          console.log(err);
+          res.send({status:false, message: "Problem with the password"})
+        }
+      })
+    }else{
+      res.send({status:false, message:'incorrect email or password'})
+    }
+  }).catch((err)=>{
+    console.log(err);
+  });
+
+};
+
+const dashboard = (req,res)=>{
+  // console.log("dashboard works ");
+  let  token = req.headers.authorization.split(" ")[1]
+  jwt.verify(token, secret, ((err, result)=>{
+    console.log(result);
+   
+    if (err) {
+      res.send({status:false, message: "Hey it seems you used the wrong oken not correct "})
+    }else{
+      res.send({status:true, message: "Token verified", result})
+    }
+  }))
+}
+
+
+module.exports = {userWelcome, registerUser, signUp,authenticateUser,  dashboard }
 
--- Routes/user.route.js
+++ Routes/user.route.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const router = express.Router()
-const {userWelcome, registerUser, signUp} = require("../Controllers/user.controller")
+const {userWelcome, registerUser, signUp, authenticateUser, dashboard} = require("../Controllers/user.controller")
 
 
 router.get("/welcome", userWelcome)
 router.post("/register", registerUser)
 router.post("/newuser", signUp )
+router.post("/login", authenticateUser)
+router.post("/dashboard", dashboard)
 module.exports = router 
